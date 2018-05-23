76c9ad79a490a1f83dc16ac0d37a318a235b48c3
Biagio Distefano <biagiodistefano92@gmail.com>
2018-05-24 00:20:42 +0200

    porcoddio

--- elsagest/elsagest/settings.py
+++ elsagest/elsagest/settings.py
@@ -169,4 +169,4 @@ LOGIN_REDIRECT_URL = '/home/'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
